22cb7781ca83347c401c8ddf9393cdfd5ab653ca
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-03-23 13:06:51 +0700

    base pages router

--- src/app/components/testimonials/Testimonials.jsx
+++
@@ -1,9 +0,0 @@
-'use client'
-import React from 'react';
-import './testimonials.css'
-const Testimonials = () => {
-    return (
-        <div id='testimonials'>Testimonials</div>
-      )
-}
-export default Testimonials
