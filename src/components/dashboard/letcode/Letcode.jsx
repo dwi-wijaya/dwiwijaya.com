ff9d5f7d979f6791529ba0c5422bfe644d942e26
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-03-27 23:19:02 +0700

    fix: leetcode data coalescing

--- src/components/dashboard/letcode/Letcode.jsx
+++ src/components/dashboard/letcode/Letcode.jsx
@@ -5,10 +5,10 @@ const Letcode = ({ style, data }) => {
   return (
     <div className='leetcode__section'>
       <div className={style.dashboard__cards}>
-        <DashboardCard style={style} title='Acceptance Rate' value={data.acceptanceRate} unit='%' />
-        <DashboardCard style={style} title='Contribution Points' value={data.contributionPoints} />
-        <DashboardCard style={style} title='Total Solved' value={data.totalSolved} />
-        <DashboardCard style={style} title='Easy - Medium - Hard' value={[data.easySolved, data.mediumSolved, data.hardSolved]} isMultiple={true} />
+        <DashboardCard style={style} title='Acceptance Rate' value={data.acceptanceRate || 0} unit='%' />
+        <DashboardCard style={style} title='Contribution Points' value={data.contributionPoints || 0} />
+        <DashboardCard style={style} title='Total Solved' value={data.totalSolved || 0} />
+        <DashboardCard style={style} title='Easy - Medium - Hard' value={[data.easySolved || 0, data.mediumSolved || 0, data.hardSolved || 0]} isMultiple={true} />
       </div>
     </div>
   )
