bcc7e9f76e550d0c32fd690f24454f051c0dee92
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-03-29 04:53:21 +0700

    reset: Restart project from scratch

--- src/constants/github.js
+++
@@ -1,9 +0,0 @@
-export const GITHUB_ACCOUNTS = [
-  {
-    username: "dwi-wijaya",
-    token: process.env.GITHUB_TOKEN_PERSONAL,
-    endpoint: "/api/github?type=personal",
-    type: "personal",
-    is_active: true,
-  },
-];
--- src/services/fetcher.js
+++
@@ -1,3 +0,0 @@
-import axios from 'axios'
-
-export const fetcher = (url) => axios.get(url).then(response => response.data)
