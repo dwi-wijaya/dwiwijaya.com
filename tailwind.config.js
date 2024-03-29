2a40eebbcc545004b87fc818c115679cf297a494
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-03-29 09:48:59 +0700

    config: tailwind and global css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        container: 'var(--container-color)',
+        stroke: 'var(--stroke-color)',
+        primary: 'var(--primary-color)',
+        title: 'var(--title-color',
+        text: 'var(--text-color)',
+        subtext: 'var(--text-secondary)',
       },
-    },
+    }
   },
   plugins: [],
 };
