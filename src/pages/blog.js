a5c874f959ca09c7df9a8de7acb76c71970e7265
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-03-30 04:14:20 +0700

    page: blog

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import Container from "@/components/layout/Container"
+import Blog from "@/components/views/Blog"
+
+const blog = () => {
+    return (
+        <Container>
+            <Blog />
+        </Container>
+    )
+}
+
+export default blog
