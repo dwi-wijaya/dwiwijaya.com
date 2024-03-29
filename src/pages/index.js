4caa1f6f5c7c9b21011da767dd612599f12121e5
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-03-29 21:33:18 +0700

    style: add border homepage header background

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
       </span>
       Open for collabs ! </div>
 
-      <div className="z-[-1] bg-container top-0 left-0 w-full h-[30vh] bg-endless-clouds"></div>
+      <div className="z-[-1] bg-container border border-b border-stroke top-0 left-0 w-full h-[30vh] bg-endless-clouds"></div>
       <section data-aos="fade-right" data-section className="relative p-[15px] pl-5">
         <Homepage />
       </section>
