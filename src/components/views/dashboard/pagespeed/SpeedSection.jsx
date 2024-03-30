471cee98519fbb07dafd15abdbdc63c29d782ed2
Dwi Wijaya <{ID}+{username}@86275109+dwi-wijaya@users.noreply.github.com>
2024-03-30 20:40:31 +0700

    style: hide scrollbar pagespeed

--- src/components/views/dashboard/pagespeed/SpeedSection.jsx
+++ src/components/views/dashboard/pagespeed/SpeedSection.jsx
@@ -10,7 +10,7 @@ export default function SpeedSection({ data, isLoading, style }) {
     if (isLoading) return <LoadingSpeedInsight style={style} />
 
     return (
-        <div className="my-2 flex items-center justify-start text-xs gap-4 overflow-y-hidden">
+        <div className="my-2 flex items-center justify-start text-xs gap-4 overflow-y-hidden scrollbar-hide">
             {categoriesInArray.map(category => (
                 <div key={category.id} className="mt-2 flex flex-col items-center justify-start text-xs gap-3">
                     <h3 className='whitespace-nowrap'>{category.title}</h3>
