4cf690acea47084496158ed61f79a756862e79c4
Dwi Wijaya <{ID}+{username}@86275109+dwi-wijaya@users.noreply.github.com>
2024-03-30 20:22:47 +0700

    style: skeleton pagespeed

--- src/components/elements/LoadingSpeedInsight.jsx
+++ src/components/elements/LoadingSpeedInsight.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 export default function LoadingSpeedInsight() {
   return (
-    <div className="my-4 flex items-center justify-start text-xs gap-4 overflow-y-hidden">
+    <div className="my-2 flex items-center justify-start text-xs gap-4 overflow-y-hidden">
       {[1, 2, 3, 4].map(item => (
-        <div key={item} className="flex w-max flex-col items-center justify-center space-y-2 text-center">
-          <div className="h-3 w-16 animate-pulse rounded-full bg-container md:w-[82px]"></div>
-          <div className="h-12 w-12 animate-pulse rounded-full bg-container p-[6px] md:h-16 md:w-16">
-            <div className="flex h-full w-full items-center justify-center rounded-full bg-transparent">
-              <i className='bx bx-dots-horizontal-rounded' ></i>
+        <div key={item} className="flex w-max gap-3 flex-col items-center justify-center space-y-2 text-center">
+          <div className="mt-2 h-3 w-16 animate-pulse rounded-full bg-container md:w-[82px]"></div>
+          <div className="h-16 !mt-0 !mb-1 w-16 animate-pulse rounded-full bg-transparent p-2 md:h-20 md:w-20">
+            <div className="flex h-full w-full items-center outline outline-container !outline-[0.5rem] justify-center rounded-full bg-transparent">
+              <i className=' text-xl bx bx-dots-horizontal-rounded' ></i>
             </div>
           </div>
         </div>
