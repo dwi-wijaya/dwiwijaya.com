6569aa02c76b0bfdfdafd26821f7e12f11bbf7da
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-03-31 00:44:42 +0700

    fix: disable aos transition homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       <NextSeo title='Dwi Wijaya - Personal Website' />
       
       <div className="z-[-1] bg-container border border-b border-stroke top-0 left-0 w-full h-[30vh] bg-endless-clouds"></div>
-      <section data-aos="fade-right" data-section className="relative p-[15px] pl-5 mx-auto max-w-[1024px]">
+      <section data-section className="relative p-[15px] pl-5 mx-auto max-w-[1024px]">
         <Homepage />
       </section>
     </>
