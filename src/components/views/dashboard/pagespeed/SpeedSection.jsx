944f3334f12fccdb08b356de271d71abaf7e0449
Dwi Wijaya <{ID}+{username}@86275109+dwi-wijaya@users.noreply.github.com>
2024-03-30 20:02:28 +0700

    fix: pagespeed loading

--- src/components/views/dashboard/pagespeed/SpeedSection.jsx
+++ src/components/views/dashboard/pagespeed/SpeedSection.jsx
@@ -7,7 +7,7 @@ export default function SpeedSection({ data, isLoading, style }) {
     const categories = data?.lighthouseResult?.categories || {}
     const categoriesInArray = Object.keys(categories).map(key => ({ ...categories[key] }))
 
-    if (isLoading || data) return <LoadingSpeedInsight style={style} />
+    if (isLoading) return <LoadingSpeedInsight style={style} />
 
     return (
         <div className="my-2 flex items-center justify-start text-xs gap-4 overflow-y-hidden">
