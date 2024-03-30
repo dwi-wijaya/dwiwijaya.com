53c2fbe151f3a27ea8336eebf520f7a820267e2a
Dwi Wijaya <{ID}+{username}@86275109+dwi-wijaya@users.noreply.github.com>
2024-03-30 21:43:47 +0700

    style: container component margin and max width

--- src/components/layout/Container.jsx
+++ src/components/layout/Container.jsx
@@ -3,7 +3,9 @@ import { ReactNode } from 'react';
 const Container = ({ children, className = '', ...others }) => {
   return (
     <div className={`mt-[4.5rem] mb-10 p-6 ${className} `} {...others}>
-      {children}
+      <section className='mx-auto max-w-[1024px]'>
+        {children}
+      </section>
     </div>
   );
 };
