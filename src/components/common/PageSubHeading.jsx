bf0bc3b465a9d561fdfbccdff0021e3b8b5446fa
Dwi Wijaya <{ID}+{username}@86275109+dwi-wijaya@users.noreply.github.com>
2024-03-30 20:39:52 +0700

    style: pagesubheading

--- src/components/common/PageSubHeading.jsx
+++ src/components/common/PageSubHeading.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const PageSubHeading = ({ title, description, icon, link, linkText }) => {
   return (
     <>
-      <h3 className="flex items-center gap-2 font-medium text-lg leading-4">
+      <h3 className="flex items-center gap-2 font-medium text-lg">
         {icon && <i className={icon}></i>}{title}
       </h3>
-      <div className="text-base text-subtext mb-3 flex justify-between leading-relaxed">
-        <p className="section__subtitle-text">
+      <div className="flex flex-col md:flex-row  text-subtext mb-3 flex justify-between leading-relaxed">
+        <p className="text-sm leading-5">
           {description}
         </p>
         {link && linkText && <a target="_blank" className="section__subtitle-link" href={link}>{linkText}</a>}
