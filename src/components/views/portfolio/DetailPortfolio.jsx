d878f9d2947f9b5f3c0e34daa5c4f39c526a65b3
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-04-13 20:14:36 +0700

    remove: bottom hr detail portfolio

--- src/components/views/portfolio/DetailPortfolio.jsx
+++ src/components/views/portfolio/DetailPortfolio.jsx
@@ -6,7 +6,7 @@ import Link from 'next/link'
 import React, { useState } from 'react'
 import "@uiw/react-md-editor/markdown-editor.css";
 import "@uiw/react-markdown-preview/markdown.css";
-import { PORTFOLIO_CATEGORIES, PORTFOLIO_CATEGORIES_ICON, PORTFOLIO_TYPES,PORTFOLIO_TYPES_ICON } from '@/constants/data/portfolio';
+import { PORTFOLIO_CATEGORIES, PORTFOLIO_CATEGORIES_ICON, PORTFOLIO_TYPES, PORTFOLIO_TYPES_ICON } from '@/constants/data/portfolio';
 
 const MDEditorPreview = dynamic(
     () => import("@uiw/react-markdown-preview").then((module) => module.default),
@@ -22,25 +22,25 @@ const DetailPortfolio = ({ portfolio }) => {
             <p className='text-sm text-subtext leading-5'>{portfolio.excerpt}</p>
             <hr className="hr" />
 
-                <div className="flex item-center gap-2 justify-center sm:justify-normal">
-                    {portfolio.demoLink &&
-                        <a href={portfolio.demoLink} className="badge text-sm mb-5">
-                            <i className='bx bx-link-external'></i> Live Demo
-                        </a>
-                    }
-                    {portfolio.githubLink &&
+            <div className="flex item-center gap-2 justify-center sm:justify-normal">
+                {portfolio.demoLink &&
+                    <a href={portfolio.demoLink} className="badge text-sm mb-5">
+                        <i className='bx bx-link-external'></i> Live Demo
+                    </a>
+                }
+                {portfolio.githubLink &&
                     <a href={portfolio.githubLink} className={`badge text-sm mb-5`}>
                         <i className='bx bxl-github'></i> Source Code
                     </a>
-                    }
-                </div>
+                }
+            </div>
 
-                <Image alt={portfolio.name} src={portfolio.thumbnail} width={0}
-                    height={0}
-                    sizes="100vw"
-                    className='rounded-md'
-                    style={{ width: '100%', height: 'auto' }}
-                />
+            <Image alt={portfolio.name} src={portfolio.thumbnail} width={0}
+                height={0}
+                sizes="100vw"
+                className='rounded-md'
+                style={{ width: '100%', height: 'auto' }}
+            />
             <div className='flex gap-2 items-center mt-4 overflow-y-hidden scrollbar-hide' >
                 {portfolio.skill.map((skill, index) => (
                     <span className="badge text-sm " key={index}>
@@ -51,14 +51,11 @@ const DetailPortfolio = ({ portfolio }) => {
                     </span>
                 ))}
             </div>
-                <hr className='hr' />
+            <hr className='hr' />
             <MDEditorPreview
                 source={portfolio.content}
-                className="md:p-4 rounded-lg"
+                className="md:p-4 rounded-lg mb-3"
             />
-            <hr className="hr" />
-            {console.log(PORTFOLIO_CATEGORIES)
-            }
             <p className='text-sm text-subtext flex items-center gap-2'><i className='bx bx-chevron-right'></i> Category:  <i className={PORTFOLIO_CATEGORIES_ICON[portfolio.category]}></i> {PORTFOLIO_CATEGORIES[portfolio.category]}</p>
             <p className='text-sm text-subtext flex items-center gap-2'><i className='bx bx-chevron-right'></i> Type:  <i className={PORTFOLIO_TYPES_ICON[portfolio.type]}></i> {PORTFOLIO_TYPES[portfolio.type]} </p>
         </div>
