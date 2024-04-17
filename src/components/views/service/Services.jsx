e222a2e1b27c006f6b7a830e81a72e27f88a7065
Dwi Wjiaya <dev.dwiwijaya@gmail.com>
2024-04-17 11:02:42 +0700

    style: motion item services

--- src/components/views/service/Services.jsx
+++ src/components/views/service/Services.jsx
@@ -1,17 +1,25 @@
 import { ServicesData } from "@/constants/data/services";
 import PageHeading from "../../common/PageHeading";
+import { motion } from "framer-motion";
 
 const Services = () => {
   return (
     <>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-x-4 gap-y-4">
-        {ServicesData.map(({ id, icon, title, description }) => {
+        {ServicesData.map(({ id, icon, title, description,  },index) => {
           return (
-            <div data-aos="fade-right" className="card text-center !p7-6 ring-0 hover:ring-1 hover:ring-primary dark:hover:ring-1 dark:hover:ring-[#ff9f8c68]" key={id}>
+            <motion.div
+              initial={{ opacity: 0, scale: 0.9 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 0.05, delay: index * 0.1 }}
+              className="card text-center !p7-6 ring-0 hover:ring-1 hover:ring-primary dark:hover:ring-1 dark:hover:ring-[#ff9f8c68]"
+              key={id}
+              data-aos="fade-right"
+            >
               <i className={`text-5xl text-primary bx bx-${icon}`}></i>
               <h3 className="my-3 font-semibold">{title}</h3>
               <p className="text-sm font-light text-subtext">{description}</p>
-            </div>
+            </motion.div>
           );
         })}
       </div>
