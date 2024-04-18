68aafe323b0c60e73a046e782c94fc298b215b81
Dwi Wjiaya <dev.dwiwijaya@gmail.com>
2024-04-18 14:04:56 +0700

    change: pagespeed web url

--- src/constants/pagespeed.js
+++ src/constants/pagespeed.js
@@ -1,4 +1,4 @@
-export const PAGESPEED_PAGEWEB_URL = 'http://dwi-wijaya.vercel.app' 
+export const PAGESPEED_PAGEWEB_URL = 'http://dwiwijaya.vercel.app' 
 export const PAGESPEED_URL = 'https://pagespeed.web.dev/'
 export const PAGESPEED_BASE = 'https://www.googleapis.com/pagespeedonline/v5/runPagespeed?category=accessibility&category=performance&category=best-practices&category=seo' 
 export const PAGESPEED_API = PAGESPEED_BASE + '&url=' + PAGESPEED_PAGEWEB_URL
