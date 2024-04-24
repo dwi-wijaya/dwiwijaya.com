f8910454198b0a9f3daabb3076f2ff7347574fcc
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-04-24 21:32:46 +0700

    fix: style and responsive sidebar

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -19,7 +19,7 @@ const Sidebar = () => {
 
     useEffect(() => {
         const handleClickOutside = (event) => {
-            if (sidebarRef.current &&  !sidebarRef.current.contains(event.target)) {
+            if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
                 setToggle(false);
             }
         };
@@ -27,7 +27,17 @@ const Sidebar = () => {
 
 
         document.addEventListener("mousedown", handleClickOutside);
+        const handleResize = () => {
+            if (window.innerWidth <= 1024) {
+                setToggle(false); // Jika lebar layar <= 767px, kelas dihapus
+            }
+        };
+
+        window.addEventListener('resize', handleResize); // Tambahkan event listener untuk resize
+
+        // Hapus event listener saat komponen di-unmount
         return () => {
+            window.removeEventListener('resize', handleResize);
             document.removeEventListener("mousedown", handleClickOutside);
         };
     }, [toggle]);
@@ -42,7 +52,7 @@ const Sidebar = () => {
 
     return (
         <>
-            <aside ref={sidebarRef} className={`${toggle && '!left-0'} fixed -left-[80px] lg:left-0 top-0 bg-container border-r border-stroke p-6 w-20 min-h-screen flex flex-col justify-between text-center transition-3s z-10`}>
+            <aside ref={sidebarRef} className={`${toggle && '!left-0'} fixed -left-[80px] lg:left-0 top-0 bg-container border-r border-stroke p-6 w-20 min-h-screen flex flex-col justify-between text-center transition-3s z-10 shadow-sm`}>
                 <Link aria-label='go home' href="/" className="nav__logo">
                     <Image src={theme == 'dark' ? LogoDark : LogoLight} alt="Dwi-logo" />
                 </Link>
@@ -51,7 +61,7 @@ const Sidebar = () => {
                         <ul className="flex flex-col items-center gap-y-7">
                             {MENU_ITEMS.map((item, index) => (
                                 <li key={index} className='nav__item '>
-                                    <Link href={item.href} title={item.label} className={`${pathname === item.href ? 'active p-2 border border-primary !text-primary rounded-md' : ''} hover:text-primary text-2xl text-text font-bold flex items-center h-full transition-3s`}>
+                                    <Link href={item.href} title={item.label} className={`${pathname === item.href ? 'active px-3 py-2 border border-[#febe82] dark:border-[#ff77134c] bg-[#ff985c4d] dark:bg-[#4d4036] !text-[#d42400] dark:!text-[#ffab00] rounded-md' : ''} hover:text-primary text-2xl text-text font-bold flex items-center h-full transition-3s`}>
                                         <i className={item.iconClass}></i>
                                     </Link>
                                 </li>
