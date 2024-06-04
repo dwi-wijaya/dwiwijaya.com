4cc076ee3dd2054d37860f80c70e1cdfd374cca9
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-06-04 22:46:39 +0700

    style: theme toggle active radius

--- src/components/toggles/ThemeToggle.jsx
+++ src/components/toggles/ThemeToggle.jsx
@@ -31,7 +31,7 @@ const ThemeToggle = () => {
                 />
                 <div className="slider bg-background peer-checked:bg-background flex rounded-2xl justify-between items-center w-full h-full  transition-all duration-300 relative">
                     <span
-                        className={`absolute left-0 w-1/2 h-full bg-container border border-stroke rounded-2xl transition-all duration-300 transform ${theme === "dark" ? 'translate-x-full' : ''}`}
+                        className={`absolute left-0 w-1/2 h-full bg-container border border-stroke rounded-xl transition-all duration-300 transform ${theme === "dark" ? 'translate-x-full' : ''}`}
                         style={{ content: '""', zIndex: 1 }}
                     />
                     <p className={`z-10 pl-4 leading-4 flex items-center gap-2 text-slate-500 ${theme == 'light' ? '!text-slate-800' : ''}`}><i className="bx bxs-sun"></i>Light</p>
