eff5dff5cd0b340898d5d004db900cf6c66fa250
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-06-06 05:58:51 +0700

    style: skill item margin

--- src/components/views/skill/SkillItem.jsx
+++ src/components/views/skill/SkillItem.jsx
@@ -1,6 +1,6 @@
 const SkillItem = ({ skill, index }) => {
     return (
-        <div className=" hover:ring-offset-2  ring-offset-background hover:ring-2 hover:ring-stroke hover:!border-slate-300 dark:hover:!border-slate-500 transition-3s inline-flex items-center justify-center text-lg gap-2 m-2 border border-stroke bg-container rounded-lg px-4 py-2 transition-3s" key={index}>
+        <div className="hover:ring-1 sm:hover:ring-1 ring-offset-2  ring-offset-background  ring-stroke hover:!border-slate-300 dark:hover:!border-slate-500 text-sm sm:text-lg inline-flex items-center justify-center  gap-2 m-1 sm:m-2 border border-stroke bg-container rounded-lg px-4 py-2 transition-3s" key={index}>
             <i dangerouslySetInnerHTML={{__html: skill.icon}} />
             {skill.name}
         </div>
