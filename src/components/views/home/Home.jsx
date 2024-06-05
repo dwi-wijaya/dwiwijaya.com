f5b99944cb173745ef283ec11ac39b0d6d9d4865
Dwi Wjiaya <dev.dwiwijaya@gmail.com>
2024-06-05 15:45:36 +0700

    add: homepage base in component

--- src/components/views/home/Home.jsx
+++ src/components/views/home/Home.jsx
@@ -6,14 +6,19 @@ import Profile from '@/assets/avatar-profile.jpg'
 import HomeSocials from './HomeSocials'
 const Home = () => {
     return (
-        <div className='absolute -top-[175%] w-[calc(100%-40px)]'>
-            <Image
-                width={108}
-                height={108}
-                src={Profile}
-                alt="Avatar Dwi"
-                className="outline outline-[.5rem] outline-background rounded-2xl mb-8"
-            />
+        <div className='absolute -top-[175%] w-[calc(100%-40px)] '>
+            <div className="flex justify-between gap-2 items-end  mb-8">
+                <Image
+                    width={108}
+                    height={108}
+                    src={Profile}
+                    alt="Avatar Dwi"
+                    className="outline outline-[.5rem] outline-background rounded-2xl"
+                />
+                <div className="badge text-sm h-fit mb-1 w-full sm:w-fit text-end !gap-3 !py-[.6rem] !rounded-xl">
+                    <i className="fad fa-location-dot text-primary"></i> Based in Yogyakarta, ID
+                </div>
+            </div>
             <h2 className='text-xl text-subtext mb-0 flex items-center gap-1 '>Hi there<span className="animate-waving-hand">👋</span> </h2>
             <div className="flex gap-2 self-center items-center mb-1">
                 <h1 className="font-bold text-3xl text-text">
