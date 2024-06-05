bad0d946f675d62072bcb001221d51ac1c12f47c
Dwi Wjiaya <dev.dwiwijaya@gmail.com>
2024-06-05 12:01:09 +0700

    style: homepage social to badge

--- src/components/views/home/HomeSocials.jsx
+++ src/components/views/home/HomeSocials.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 
 const HomeSocials = () => {
     return (
-        <div className="flex gap-3 mt-3">
+        <div className="flex gap-3 mt-6">
             {Socials.map((social, index) => (
-                <a aria-label={`go to ${social.label}`} key={index} href={social.link} target='_blank' className="h-fit flex text-xl text-text hover:text-primary">
-                    <i className={`bx bx-tada bxl-${social.boxIcon}`}></i>
+                <a aria-label={`go to ${social.label}`} key={index} href={social.link} target='_blank' className="h-fit flex text-xl text-text hover:text-primary badge items-center">
+                    <i className={`bx bxl-${social.boxIcon}`}></i>
                 </a>
             ))}
         </div>
