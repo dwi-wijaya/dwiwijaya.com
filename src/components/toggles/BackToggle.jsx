04cafaa3179d43b2a6795175d38c022eac01ef4d
Dwi Wjiaya <dev.dwiwijaya@gmail.com>
2024-06-06 14:46:44 +0700

    fix: position back btn portfolio

--- src/components/toggles/BackToggle.jsx
+++ src/components/toggles/BackToggle.jsx
@@ -8,7 +8,7 @@ const BackToggle = () => {
         router.back();
     };
     return (
-        <button onClick={handleGoBack} aria-label="back toggle" className='group-[.sidebar-expanded]/main:-left-[200px] active:ring toggle lg:left-[105px] left-[70px]'>
+        <button onClick={handleGoBack} aria-label="back toggle" className='group-[.sidebar-expanded]/main:-left-[200px] active:ring toggle lg:left-[17rem] left-[70px] !text-primary'>
             <i className='text-xl bx bx-chevron-left' /> Back
         </button>
     );
