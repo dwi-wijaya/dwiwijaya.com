f5291d9f0f4dfc83093e8ab7fd3e6a2eff95529c
Dwi Wjiaya <dev.dwiwijaya@gmail.com>
2024-06-06 14:51:41 +0700

    style: change icon back btn

--- src/components/toggles/BackToggle.jsx
+++ src/components/toggles/BackToggle.jsx
@@ -8,8 +8,8 @@ const BackToggle = () => {
         router.back();
     };
     return (
-        <button onClick={handleGoBack} aria-label="back toggle" className='group-[.sidebar-expanded]/main:-left-[200px] active:ring toggle lg:left-[17rem] left-[70px] !text-primary'>
-            <i className='text-xl bx bx-chevron-left' /> Back
+        <button onClick={handleGoBack} aria-label="back toggle" className='group-[.sidebar-expanded]/main:-left-[200px] active:ring toggle gap-2 lg:left-[17rem] left-[70px] !text-primary'>
+            <i className='fad fa-circle-arrow-left' /> Back
         </button>
     );
 };
