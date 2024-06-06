47574d4752c201fc9f76d9ee2e31aa0c8e1bf46c
Dwi Wjiaya <dev.dwiwijaya@gmail.com>
2024-06-06 09:40:40 +0700

    update: about page

--- src/components/views/about/About.jsx
+++ src/components/views/about/About.jsx
@@ -3,20 +3,35 @@ import Image from 'next/image'
 import Info from './Info'
 import PageSubHeading from '../../common/PageSubHeading'
 
-const About = ({skills, certificates,about}) => {
+const About = ({ skills, certificates, about }) => {
   return (
     <>
-      <div className="grid grid-cols-1 lg:grid-cols-2 items-center gap-x-16 gap-y-8">
-        <Image src={Freelance} alt="Freelance" className='justify-self-center w-[420px] lg:w-full' />
-        <div className="text-center lg:text-start">
-          <Info about={about}/>
-          <p className='p-2 mb-3 text-subtext text-md'>
-            {about.description}
-          </p>
-          <a aria-label='download cv' href={about.resume.replace('preview','edit')} target='_blank' className="btn !inline-flex !gap-3">
-            <i className="fad fa-file-lines"></i>
-            Download CV</a>
-        </div>
+      <div className="">
+        <p className='mb-6'>
+          Hi, I&apos;m Dwi Wijaya, a passionate full-stack web developer with a love for creating dynamic and user-friendly websites.
+          With over 2 years of experience in the industry, I specialize in front-end development and have a keen eye for design and detail.
+          My goal is to create pixel-perfect websites that not only meet the client&apos;s needs but also exceed their expectations.
+        </p>
+
+        <PageSubHeading icon={'fad fa-route'} title="My Journey" />
+        <p className='mb-3'>My career in web development began during my college years at the High School,
+          where I majored in Software Engineering. My first job was at a small software house,
+          where I learned the ropes of web development and discovered my passion for coding.
+          Over the years, I have had the opportunity to work on numerous exciting projects.
+          Each experience has contributed to my growth as a developer and has helped me hone my skills.</p>
+
+        <PageSubHeading icon={'fad fa-lightbulb'} title="Values and Philosophy" />
+        <p className='mb-3'>I believe in continuous learning and improvement.
+          I am driven by a desire to create websites that are not only functional but also aesthetically pleasing.
+          My work is guided by the principles of simplicity, usability, and accessibility.
+          I strive to keep up with the latest trends and technologies in web development to ensure that my skills are always up to date.</p>
+
+        <PageSubHeading icon={'fad fa-laugh-beam'} title="Fun Facts" />
+
+        <ul>
+          <li>I don&apos;t have any fun facts yet 😭.</li>
+        </ul>
+
       </div>
     </>
   )
