a71831437f29f4cb74f164dcba6b46fe515ff6ab
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-06-07 20:47:40 +0700

    add: dashboard preview title category speed insight

--- src/components/elements/LoadingSpeedInsight.jsx
+++ src/components/elements/LoadingSpeedInsight.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
 export default function LoadingSpeedInsight() {
+  const DummyCategory = [{ 'title': 'Performance', }, { 'title': 'Accessibility', }, { 'title': 'Best Practices', }, { 'title': 'SEO', }]
   return (
     <div className="my-2 flex items-center justify-start text-xs gap-4 overflow-y-hidden scrollbar-hide">
-      {[1, 2, 3, 4].map(item => (
-        <div key={item} className="flex w-max gap-3 flex-col items-center justify-center space-y-2 text-center">
-          <div className="mt-2 h-3 w-20 animate-pulse rounded-full bg-slate-300 dark:bg-container md:w-[82px]"></div>
-          <div className="h-16 !mt-0 !mb-1 w-16 animate-pulse rounded-full bg-transparent p-2 md:h-20 md:w-20">
+      {DummyCategory.map(item => (
+        <div key={item} className="mt-2 flex flex-col items-center justify-start text-xs gap-3">
+          <div className="whitespace-nowrap">{item.title}</div>
+          <div className="h-16 !mt-0 w-16 animate-pulse rounded-full bg-transparent p-2 md:h-20 md:w-20">
             <div className="flex h-full w-full items-center outline outline-slate-300 dark:outline-container !outline-[0.5rem] justify-center rounded-full bg-transparent">
               <i className='outline-slate-400 dark:outline-slate-400 text-xl bx bx-dots-horizontal-rounded' ></i>
             </div>
