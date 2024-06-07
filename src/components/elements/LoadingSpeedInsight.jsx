be310da6697645e6f7d6a3f90526e420566f67b0
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-06-07 21:40:44 +0700

    style: add preview category speed insight

--- src/components/elements/LoadingSpeedInsight.jsx
+++ src/components/elements/LoadingSpeedInsight.jsx
@@ -4,8 +4,8 @@ export default function LoadingSpeedInsight() {
   const DummyCategory = [{ 'title': 'Performance', }, { 'title': 'Accessibility', }, { 'title': 'Best Practices', }, { 'title': 'SEO', }]
   return (
     <div className="my-2 flex items-center justify-start text-xs gap-4 overflow-y-hidden scrollbar-hide">
-      {DummyCategory.map(item => (
-        <div key={item} className="mt-2 flex flex-col items-center justify-start text-xs gap-3">
+      {DummyCategory.map((item,index) => (
+        <div key={item.title} className="mt-2 flex flex-col items-center justify-start text-xs gap-3">
           <div className="whitespace-nowrap">{item.title}</div>
           <div className="h-16 !mt-0 w-16 animate-pulse rounded-full bg-transparent p-2 md:h-20 md:w-20">
             <div className="flex h-full w-full items-center outline outline-slate-300 dark:outline-container !outline-[0.5rem] justify-center rounded-full bg-transparent">
