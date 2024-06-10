8dc960623e136596e2f0feb573a82820e19d73db
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-06-10 21:21:53 +0700

    fix: umami event portfolio category

--- src/components/views/portfolio/PortfolioCategory.jsx
+++ src/components/views/portfolio/PortfolioCategory.jsx
@@ -5,7 +5,7 @@ const PortfolioCategory = ({ filter, active }) => {
         <div className="flex gap-4 mb-6 justify-center sm:justify-start">
             {portfolioCategory.map((category, index) => (
                 <div key={index} className="category">
-                    <button data-umami-event="Click Portfolio Category" onClick={() => filter(category.slug)} title={category.label} className={`text-lg sm:text-base badge !px-4 !py-3 sm:!px-3 sm:!py-2 ${active === category.slug ? '!text-primary' : ''}`}>
+                    <button data-umami-event={`Click Portfolio Category - ${category.label}`} onClick={() => filter(category.slug)} title={category.label} className={`text-lg sm:text-base badge !px-4 !py-3 sm:!px-3 sm:!py-2 ${active === category.slug ? '!text-primary' : ''}`}>
                         <i className={category.icon}></i>
                         <span className='hidden sm:block'>{category.label}</span>
                     </button>
