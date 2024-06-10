6fe4fb6b151aed9348796d2d4922c1008c26c426
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-06-10 21:31:13 +0700

    fix: pagespeed constant site url

--- src/constants/pagespeed.js
+++ src/constants/pagespeed.js
@@ -1,5 +1,4 @@
-export const PAGESPEED_PAGEWEB_URL = 'https://dwiwijaya.vercel.app' 
 export const PAGESPEED_URL = 'https://pagespeed.web.dev/'
-export const PAGESPEED_BASE = 'https://www.googleapis.com/pagespeedonline/v5/runPagespeed?category=accessibility&category=performance&category=best-practices&category=seo' 
-export const PAGESPEED_API = PAGESPEED_BASE + '&url=' + PAGESPEED_PAGEWEB_URL
+export const PAGESPEED_BASE = 'https://www.googleapis.com/pagespeedonline/v5/runPagespeed?category=accessibility&category=performance&category=best-practices&category=seo'
+export const PAGESPEED_API = PAGESPEED_BASE + '&url=' + process.env.NEXT_PUBLIC_SITE_URL
 
