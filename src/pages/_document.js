4d8711470d211846f21f22efe104b6ecc56281f9
Dwi Wjiaya <dev.dwiwijaya@gmail.com>
2024-06-10 11:11:48 +0700

    config: remove google console tag document

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,8 +7,6 @@ export default function Document() {
         <link rel="manifest" href="/manifest.json" />
         <link rel="icon" href="/logo2.png" sizes="any" />
         <meta name="theme-color" content="#ff774d" />
-        <meta name="google-site-verification" content="vSWLUOfzaGmniX0J55YW3eB1xePdFkyD6_jSK6___Ww" />
-       
       </Head>
       <body>
         <Main />
