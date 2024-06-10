7737389a7571b11c4014ed5a25f027d483cacc2d
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-06-10 19:42:23 +0700

    config: update script tracking code umami

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
         <link rel="manifest" href="/manifest.json" />
         <link rel="icon" href="/logo2.png" sizes="any" />
         <meta name="theme-color" content="#ff774d" />
-        <script defer src="https://umami-dwiwijaya.vercel.app/script.js" data-website-id="7e71a6f6-f863-400e-806f-1af37e845ad1"></script>
+        <script defer src="https://analytics.dwiwijaya.com/script.js" data-website-id="7e71a6f6-f863-400e-806f-1af37e845ad1"></script>
       </Head>
       <body>
         <Main />
