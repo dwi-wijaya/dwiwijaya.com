3a96cafbae43b5a3b89b5bab347bb5b5c0d29a34
Dwi Wjiaya <dev.dwiwijaya@gmail.com>
2024-06-11 11:14:28 +0700

    style: logo width sidebar

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -67,10 +67,10 @@ const Sidebar = ({ className, lastUpdate }) => {
             <aside ref={sidebarRef} className={`${className} ${toggle && '!left-0'} max-h-[100svh] scrollbar-hide overflow-x-auto fixed -left-64 lg:left-0 top-0 bg-container border border-stroke pl-0 p-6 w-64 min-h-screen flex flex-col gap-8 justify-between text-center transition-3s z-10 shadow-sm`}>
                 <div className="flex flex-col gap-8">
                     <div className="ml-6 flex flex-col gap-8">
-                        <Link data-umami-event={`Click Logo`} onClick={() => setToggle(false)} aria-label='go home' href="/" className="nav__logo flex gap-2 items-center text-xl  tracking-wider">
-                            <Image width={32} src={theme == 'dark' ? LogoDark : LogoLight} alt="Dwi-logo" />
+                        <Link data-umami-event={`Click Logo`} onClick={() => setToggle(false)} aria-label='go home' href="/" className="nav__logo flex gap-2 mt-1 items-center text-xl  tracking-wider">
+                            <Image width={52} src={theme == 'dark' ? LogoDark : LogoLight} alt="Dwi-logo" />
                         </Link>
-                        <div className="text-left mt-8">
+                        <div className="text-left mt-4 sm:mt-8">
                             <h2 className='text-2xl mb-2 leading-6 font-se'>Welcome to my website</h2>
                             <span className="text-sm text-subtext">
                                 Last update, {new Date(lastUpdate).toLocaleDateString('en-GB', {
