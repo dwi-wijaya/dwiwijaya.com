579f0844b5188d71c98013bac3e272bf7adc5119
Dwi Wjiaya <dev.dwiwijaya@gmail.com>
2024-06-14 11:13:47 +0700

    style: margin bottom container

--- src/components/layouts/partials/Container.jsx
+++ src/components/layouts/partials/Container.jsx
@@ -4,7 +4,7 @@ import Overlay from './Overlay';
 const Container = ({ children, className = '', ...others }) => {
   return (
     
-    <div className={`main group/main pb-10 p-6 pt-24 h-full ${className}`} {...others}>
+    <div className={`main group/main pb-6 p-6 pt-24 h-full ${className}`} {...others}>
       <section className='group-[.sidebar-expanded]/main:blur-[2px]  mx-auto max-w-[1024px]'>
         {children}
       </section>
