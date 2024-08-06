f2022d954d79bb3d41d81eea299b3f3a7635306c
Dwi Wjiaya <dev.dwiwijaya@gmail.com>
2024-08-06 14:36:25 +0700

    seo: image sqare test

--- next-seo.config.js
+++ next-seo.config.js
@@ -2,7 +2,7 @@ const metaImage =
   'https://firebasestorage.googleapis.com/v0/b/portfolio-dwiwijaya.appspot.com/o/meta-image.jpg?alt=media&token=45e644b5-1655-40e7-83b6-dc73bf3ecfa6';
 const metaDescription =
   'Passionate about crafting seamless web experiences, I specialize in building efficient, scalable, and attractive web applications. Bringing digital dreams to life';
-
+const metaSquareImage = 'https://zyhdjqkdvsbxnpngpvkc.supabase.co/storage/v1/object/public/seo/squre-ogimage.png'
 const defaultSEOConfig = {
   defaultTitle: 'Dwi Wijaya - Personal Website',
   description: metaDescription,
@@ -11,6 +11,12 @@ const defaultSEOConfig = {
     description: metaDescription,
     type: 'website',
     images: [
+      {
+        url: metaSquareImage,
+        alt: 'dwiwijaya.com og-image',
+        width: 600,
+        height: 600,
+      },
       {
         url: metaImage,
         alt: 'dwiwijaya.com og-image',
