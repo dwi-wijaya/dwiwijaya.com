501e34f5ac31b6e86a008efd80cb3047c287da3b
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-08-07 21:51:38 +0700

    fix: dashboardpage pageinsight badge section multilanguage

--- src/components/views/dashboard/pagespeed/Badge.jsx
+++ src/components/views/dashboard/pagespeed/Badge.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 
 import { MENU_ITEMS } from '@/constants/data/menu'
 import clsx from 'clsx'
+import { useRouter } from 'next/router';
 
 
 
 export default function BadgeSection({ active, refetch }) {
+    const { locale } = useRouter();
     const routes = MENU_ITEMS
     return (
         <div className='flex gap-2 mb-3 overflow-y-hidden scrollbar-hide'>
             {routes.map(route => (
                 <button
-                    data-umami-event={`Click Pagespeed - ${route.label}`}
+                    data-umami-event={`Click Pagespeed - ${route.label[locale]}`}
                     key={route.href}
                     className={clsx(
                         'badge !px-4',
@@ -22,7 +24,7 @@ export default function BadgeSection({ active, refetch }) {
                     onClick={() => refetch(route.href)}
                 >
                     <i className={route.iconClass}></i>
-                    {route.label}
+                    {route.label[locale]}
                 </button>
             ))}
         </div>
