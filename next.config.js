423a376ebb9843cad8193990f3e15e4aef4cd071
Dwi Wjiaya <dev.dwiwijaya@gmail.com>
2024-08-07 16:05:53 +0700

    fix: next config i18

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 const removeImports = require('next-remove-imports')();
-const { i18n } = require('./next-i18next.config');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
