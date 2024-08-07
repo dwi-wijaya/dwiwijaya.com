0bd0811d992ea6d4c3729f162c7cb06682df8fcc
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-08-07 23:01:46 +0700

    feat: multilanguage Skillsetpage

--- src/components/views/skill/Skill.jsx
+++ src/components/views/skill/Skill.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import InfiniteSkill from './InfiniteSkill'
 import PageSubHeading from '@/components/common/PageSubHeading'
 import Certificates from './Certificates'
+import { useTranslations } from 'next-intl'
 
 const Skill = ({skills,certificates}) => {
+    const t = useTranslations();
     return (
         <>
             <InfiniteSkill skills={skills} />
             <hr className="hr !my-8" />
-            <PageSubHeading title='Certificates' description='A showcase of my professional certificates .' icon='fad fa-file-certificate' />
+            <PageSubHeading title={t('Skillset.certificateTitle')} description={t('Skillset.certificateSubtitle')} icon='fad fa-file-certificate' />
             <Certificates certificates={certificates} />
         </>
     )
--- src/pages/skillset.js
+++ src/pages/skillset.js
@@ -2,19 +2,22 @@ import PageHeading from '@/components/common/PageHeading';
 import Container from '@/components/layouts/partials/Container';
 import Skill from '@/components/views/skill/Skill';
 import { fetcher } from '@/services/fetcher';
+import { useTranslations } from 'next-intl';
 import { NextSeo } from 'next-seo';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import React from 'react'
 
-const PAGE_TITLE = 'Skillset';
-const PAGE_DESCRIPTION = "Explore professional skillset and expertise";
 
 const SkillsetPage = ({ skills, certificates }) => {
 
+    const t = useTranslations();
     const router = useRouter();
     const canonicalUrl = `${process.env.NEXT_PUBLIC_SITE_URL}${router.asPath}`;
 
+    const PAGE_TITLE = t('Skillset.title');
+    const PAGE_DESCRIPTION = t('Skillset.subtitle');
+    
     return (
         <>
             <Head>
