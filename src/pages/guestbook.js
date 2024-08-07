b312447cae801eed201ba5f8786863e631daafba
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-08-07 21:15:13 +0700

    feat: multilanguage Guestbookpage

--- src/pages/guestbook.js
+++ src/pages/guestbook.js
@@ -2,16 +2,20 @@ const { default: Guestbook } = require("@/components/views/guestbook/Guestbook")
 const { useRouter } = require("next/router");
 import PageHeading from '@/components/common/PageHeading';
 import Container from '@/components/layouts/partials/Container';
+import { useTranslations } from 'next-intl';
 import { NextSeo } from 'next-seo';
 import Head from 'next/head';
 import React from 'react'
 
-const PAGE_TITLE = 'Guestbook';
-const PAGE_DESCRIPTION = "Leave whatever you like to say, suggestions, questions or anything!";
 
 const GuestbookPage = () => {
+
+    const t = useTranslations();
     const router = useRouter();
     const canonicalUrl = `${process.env.NEXT_PUBLIC_SITE_URL}${router.asPath}`;
+
+    const PAGE_TITLE = t('Guestbook.title');
+    const PAGE_DESCRIPTION = t('Guestbook.subtitle');
 
     return (
         <>
