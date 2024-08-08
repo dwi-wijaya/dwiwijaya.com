a1fd6dd11c1390f3e05ecb37d5f4867126a10abe
Dwi Wjiaya <dev.dwiwijaya@gmail.com>
2024-08-08 16:55:04 +0700

    style: text color component pageheading & pageshubheading

--- src/components/common/PageSubHeading.jsx
+++ src/components/common/PageSubHeading.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const PageSubHeading = ({ title, description, icon, link, linkText, tintIcon = true }) => {
   return (
     <>
-      <h3 className="flex items-center gap-2 font-medium text-lg">
+      <h3 className="flex items-center gap-2 font-medium text-lg text-title">
         {icon && <i className={`${icon} ${tintIcon && 'text-primary'}`}></i>}{title}
       </h3>
       <div className="flex flex-col md:flex-row  text-subtext mb-3 justify-between leading-relaxed">
