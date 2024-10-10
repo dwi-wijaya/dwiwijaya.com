1d0c93109a404b999e244310cd0fb92f6183543a
Dwi Wjiaya <dev.dwiwijaya@gmail.com>
2024-10-10 16:15:13 +0700

    fix: handle delete message

--- src/components/views/guestbook/GuestbookMessages.jsx
+++ src/components/views/guestbook/GuestbookMessages.jsx
@@ -7,9 +7,9 @@ export const GuestbookMessages = ({messages,onDeleteMessage,session}) => {
     return (
         <div className="rounded-lg px-1">
             <div className="space-y-5 overflow-y-auto py-4">
-            {messages.map((msg,index) => (
-                <ChatItem key={index} onDelete={onDeleteMessage} {...msg} session={session} />
-            ))}
+                {messages.map((msg, index) => (
+                    <ChatItem key={index} onDelete={onDeleteMessage} {...msg} session={session} />
+                ))}
             </div>
         </div>
     );
