d7f59a7b5a2546247bb963690d89392a3adc6ecd
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-10-11 23:01:58 +0700

    fix: ssr guestbook

--- src/pages/guestbook.js
+++ src/pages/guestbook.js
@@ -2,13 +2,14 @@ import PageHeading from "@/components/common/PageHeading"
 import Container from "@/components/layouts/partials/Container"
 import Guestbook from "@/components/views/guestbook/Guestbook"
 import { createClient } from "@/lib/server-supabase";
+import { supabase } from "@/lib/supabase";
 import { useTranslations } from "next-intl";
 import { NextSeo } from "next-seo";
 import Head from "next/head";
 import { useRouter } from "next/router";
 
 
-const GuestbookPage = ({ messages, session }) => {
+const GuestbookPage = ({ messages }) => {
 
     const t = useTranslations();
     const router = useRouter();
@@ -29,7 +30,7 @@ const GuestbookPage = ({ messages, session }) => {
                     title={PAGE_TITLE}
                     description={PAGE_DESCRIPTION}
                 />
-                <Guestbook messages={messages} session={session} />
+                <Guestbook messages={messages}/>
             </Container>
         </>
     )
@@ -37,15 +38,13 @@ const GuestbookPage = ({ messages, session }) => {
 
 export default GuestbookPage
 export const getServerSideProps = async (context) => {
-    const supabase = createClient(context)
-    const { data: session, error: sessionError } = await supabase.auth.getSession()
     const { data: messages, error: messageError } = await supabase
         .from('guestbook')
         .select('*')
         .order('created_at', { ascending: true });
     return {
         props: {
-            messages, session:session.session
+            messages,
         },
     };
 };
