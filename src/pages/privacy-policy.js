029e0e423fbac8b76eb3e35ad345ae1d6325f3eb
Dwi Wjiaya <dev.dwiwijaya@gmail.com>
2024-10-16 14:23:42 +0700

    style: privacy-policy page remove margin subtitle content

--- src/pages/privacy-policy.js
+++ src/pages/privacy-policy.js
@@ -27,30 +27,30 @@ const PrivacyPolicyPage = () => {
                 />
                 <div className='flex flex-col gap-4'>
                     <div>
-                        <div className="flex items-center space-x-2 mb-1">
-                            <HiShieldCheck className="text-green-500 text-xl" />
+                        <div className="flex items-center space-x-2">
+                            <HiShieldCheck className="text-green-500 text-xl mb-1" />
                             <h2 className="text-xl text-text font-semibold">{t('subtitles.information_collected')}</h2>
                         </div>
                         <p className="text-subtext-700">{t('information_collected')}</p>
                     </div>
                     <div>
-                        <div className="flex items-center space-x-2 mb-1">
-                            <HiShieldCheck className="text-blue-500 text-xl" />
+                        <div className="flex items-center space-x-2">
+                            <HiShieldCheck className="text-blue-500 text-xl mb-1" />
                             <h2 className="text-xl text-text font-semibold">{t('subtitles.usage')}</h2>
                         </div>
                         <p className="text-subtext-700">{t('usage')}</p>
                     </div>
                     <div>
-                        <div className="flex items-center space-x-2 mb-1">
-                            <HiShieldCheck className="text-yellow-500 text-xl" />
+                        <div className="flex items-center space-x-2">
+                            <HiShieldCheck className="text-yellow-500 text-xl mb-1" />
                             <h2 className="text-xl text-text font-semibold">{t('subtitles.security')}</h2>
                         </div>
                         <p className="text-subtext-700">{t('security')}</p>
                     </div>
 
                     <div>
-                        <div className="flex items-center space-x-2 mb-1">
-                            <HiShieldCheck className="text-red-500 text-xl" />
+                        <div className="flex items-center space-x-2">
+                            <HiShieldCheck className="text-red-500 text-xl mb-1" />
                             <h2 className="text-xl text-text font-semibold">{t('subtitles.contact')}  </h2>
                         </div>
                         <p className="text-subtext-700">{t('contact')} <a href='mailto:dev.dwiwijaya@gmail.com' className='text-primary'>dev.dwiwijaya@gmail.com</a>. </p>
